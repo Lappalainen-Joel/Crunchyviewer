1591554aed1625d50b7129275a2e344d238a4b53
Joel Lappalainen <Jopi@ren.to>
2016-12-22 17:14:29 +0200

    Update crunchyviewer.py
    - Read configuration options from 'crunchyconfig.ini'
    - Allow crunchyviewer.py to be executed with parameter.

--- crunchyviewer.py
+++ crunchyviewer.py
@@ -1,54 +1,57 @@
 #!/usr/env/python
-import requests
-import subprocess
+from requests import get
+from subprocess import call
 from bs4 import BeautifulSoup
+from sys import argv
+from configparser import ConfigParser, NoOptionError
+
+cfg = ConfigParser()
+cfg.read('crunchyconfig.ini')
+pr = cfg.get('crunchy-settings', 'pr')
+qa = cfg.get('crunchy-settings', 'qa')
+ar = cfg.get('crunchy-settings', 'ar')
 url = "http://www.crunchyroll.com"
 
-# Defines program to be executed via subprocess, with windows use full path.
-# eq. of windows version
-pr = "C:\Program Files (x86)\Livestreamer\livestreamer.exe"
-# eq. of linux version
-# pr = "livestreamer"
+try:
+    cfg.get('crunchy-settings', 'pl')
+except NoOptionError:
+    print("No player set, using default one (VLC).")
+else:
+    pl = cfg.get('crunchy-settings', 'pl')
 
-# Defines player. Use only with linux. On Windows, define default player to livestreamer config file.
-#pl = "-p mplayer"
-
-# Defines quality, in crunchyroll choices are low, mid and ultra
-qa = "ultra"
-
-# Defines hls to be passed through to player, this enables forwarding and rewinding the stream within mediaplayer.
-ar = "--player-passthrough hls"
 
 def listseries(na):
-    r = requests.get(url+"/videos/anime")
+    r = get(url + "/videos/anime")
     s = BeautifulSoup(r.content, "html.parser")
     po = s.find_all("ul", {"class": "portrait-grid cf"})
     for i in po:
-        pu = i.find_all("a",{"title":na})
-        if (len(pu) != 0):
+        pu = i.find_all("a", {"title": na})
+        if len(pu) != 0:
             u = pu[0].get("href")
             return u
-        elif (len(pu) > 1 ):
+        elif len(pu) > 1:
             print("Too many matches found, be more specific")
             exit(0)
         else:
             print("Not found, try again")
             exit(0)
 
+
 def listepisodes(se):
-    r = requests.get(url+se)
-    s = BeautifulSoup(r.content,"html.parser")
-    po = s.find_all("div",{"class": "wrapper container-shadow hover-classes"})
+    r = get(url + se)
+    s = BeautifulSoup(r.content, "html.parser")
+    po = s.find_all("div", {"class": "wrapper container-shadow hover-classes"})
     a = []
     for i in po:
         pu = i.find("a").get("href")
         a.append(pu)
     return a
 
+
 def selectepisode(ep):
     c = len(ep)
     for i in ep:
-        print(c,i)
+        print(c, i)
         c -= 1
     # Not sure why expects the value to be string.
     k = False
@@ -62,22 +65,33 @@ def selectepisode(ep):
     c2 = len(ep) - d
     return ep[c2]
 
+
 def playepisode(en):
     try:
         pl
     except:
         # This horrible creation was done because subprocess didn't understand 'ar' paramater as a separate parameter.
         # subprocess.call(pr,url+en,qa,ar)
-        subprocess.call(pr + " " + " " + url + en + " " + qa + " " + ar)
+        call(pr + " " + " " + url + en + " " + qa + " " + ar)
     else:
-        subprocess.call(pr + " " + pl + " " + url + en + " " + qa + " " + ar)
+        call(pr + " " + "-p " + pl + " " + url + en + " " + qa + " " + ar)
+
 
 def main():
-    print("Do tell the series you want to watch")
-    a = input("Case sensitive, eq. Naruto Shippuden:\n")
+    if len(argv) > 2:
+        print("Too many arguments")
+        exit(0)
+
+    elif len(argv) == 1:
+        print("Do tell the series you want to watch")
+        a = input("Case sensitive, eq. Naruto Shippuden:\n")
+    else:
+        a = argv
+
     v = listseries(a)
     v2 = listepisodes(v)
     v3 = selectepisode(v2)
     playepisode(v3)
+
 
 main()
